7fcf9d80ca8d4fe93f1acdbcb9b6176bbce54b3b
Lillian Wakefield <37185466+LillianWakefield@users.noreply.github.com>
2018-08-06 13:16:02 -0500

    Delete exercise3.py

--- classnotes/week1/Thursday Mar 1/exercise3.py
+++
@@ -1,4 +0,0 @@
-total = raw_input("Enter the total amoount ")
-percentage = raw_input("Enter the tip percentage amount ")
-new_total = int(total) * int(percentage) / 100 + int(total)
-print new_total
