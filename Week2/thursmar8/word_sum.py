d11ea9744ebca4ddbbca96114002d9e74a5b8058
Lillian Wakefield <37185466+LillianWakefield@users.noreply.github.com>
2018-12-04 19:57:50 -0800

    Delete word_sum.py

--- Week2/thursmar8/word_sum.py
+++
@@ -1,6 +0,0 @@
-user_input = raw_input("What is your phrase? ")
-user_output = {}
-split_list = user_input.split()
-for word in split_list: 
-  user_output[word]=user_input.count(word)
-print user_output
