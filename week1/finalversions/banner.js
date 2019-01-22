864f140a7d1f308a4820c0820d19035fa9b6fa77
Lillian Wakefield <37185466+LillianWakefield@users.noreply.github.com>
2019-01-22 15:56:14 -0500

    Delete banner.js

--- week1/finalversions/banner.js
+++
@@ -1,9 +0,0 @@
-function banner(){
-    var input = (prompt('What do you want to say with a fancy banner?'))
-    var len = input.length
-    fancy = Array(len+1).join('*');
-    console.log(fancy)
-    console.log(input)
-    console.log(fancy)
-}
-banner();
